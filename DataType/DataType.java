906444b65e15bfc878d57633b75e2755f306a2b6
JALAL UDDIN <103830450+JALAL-00@users.noreply.github.com>
2023-05-20 04:06:39 +0600

    Add files via upload

---
+++ DataType/DataType.java
@@ -0,0 +1,28 @@
+
+// Data type: In java there are tow data type
+// 1. Premitive data type
+// 2. Non Premitive data type
+// Non-Premitive datatype: Class, interface, Array
+// Premitive Data Type: boolean, char, byte, short, long, int, float, double 
+
+public class DataType {
+    public static void main(String[] args) {
+        boolean b = true;
+        char c = 'a';
+        byte b1 = 127;
+        short s = 127;
+        long l = 123;
+        int i = 10;
+        float f = 3.1416f;
+        double d1 = 9.110000;
+
+        System.out.println(" Boolean is "+b);
+        System.out.println(" Charater is "+c);
+        System.out.println(" Byte is "+b1);
+        System.out.println(" Short is "+s);
+        System.out.println(" long is "+l);
+        System.out.println(" int is "+i);
+        System.out.println(" float is "+f);
+        System.out.println(" double is "+d1);
+    }
+}
