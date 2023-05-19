02119edacb21316e82de9c98a124ad887b65d419
JALAL UDDIN <103830450+JALAL-00@users.noreply.github.com>
2023-05-20 04:10:32 +0600

    Add files via upload

---
+++ variable/Variable02.java
@@ -0,0 +1,10 @@
+// Java Variable Example: Add Two Numbers
+public class Variable02{
+    public static void main(String[] args) {
+        int x = 50;
+        int y = 100;
+        int sum = x + y;
+        System.out.println("Sumation is "+ sum);
+    }
+}
+
