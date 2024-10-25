c6e34852505ab3a8e8a930c3dfb33d36859a3fd1
M. JALAL <103830450+JALAL-00@users.noreply.github.com>
2024-10-25 21:21:58 +0600

    Add files via upload

---
+++ 3. variable/Variable05.java
@@ -0,0 +1,8 @@
+public class Variable05{
+    public static void main(String[] args) {
+        double d = 12.0564d;
+        int a = (int)d;
+        System.out.println(d);
+        System.out.println(a);
+    }
+}
