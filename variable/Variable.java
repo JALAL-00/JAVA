7df8cd9c48e35246c623f0d9f6529a2b84cf7928
M. JALAL <103830450+JALAL-00@users.noreply.github.com>
2024-10-25 21:21:09 +0600

    Delete variable directory

--- variable/Variable.java
+++
@@ -1,7 +0,0 @@
-// two types of data types in Java: primitive and non-primitive.
-public class Variable {
-    public static void main(String[] args) {
-        int x = 50;
-        System.out.println("Variable is "+x);
-    }
-}
--- variable/Variable03.java
+++
@@ -1,9 +0,0 @@
-// Java Variable Example: Widening
-public class Variable03{
-    public static void main(String[] args) {
-        int a = 10;
-        float f = a;
-        System.out.println(a);
-        System.out.println(f);
-    }
-}
